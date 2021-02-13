ad21064d8e61ac24418f17a6dd4dd4cd4dbd9e2f
aayush.bhankale <aayush.bhankale@scikey.ai>
2021-02-13 14:26:18 +0530

    feat: container and component structure added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Layout from './components/Layout/Layout';
+
 
 function App() {
   return (
     <div>
-    
+    <Layout>
+      <p>Cool hai</p>
+    </Layout>
     </div>
   );
 }
